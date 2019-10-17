d0ef6509e092294a8774b0183c412c68ff30ffcf
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-10-17 18:01:50 -0400

    BFT and DFT

---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+            self.size += 1
+            return self.storage.add_to_head(value)
+            
+
+
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+            
+
+    def len(self):
+        if not self.storage.head:
+            return 0
+        temp = self.storage.head
+        count = 0
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
